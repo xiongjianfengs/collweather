0402b36f3844ae40ed32435708b41d6d1a34fdc8
bear <1623495550@qq.com>
2018-01-13 21:15:02 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by xjf on 2018/1/13.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
